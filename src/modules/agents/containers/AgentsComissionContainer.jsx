faa0b977504272ec8d0924cae09e05bf19b79665
BoburUsmanoff <bobur@pixeel.co.uk>
2024-07-09 21:42:42 +0500

    fix

--- src/modules/agents/containers/AgentsComissionContainer.jsx
+++ src/modules/agents/containers/AgentsComissionContainer.jsx
@@ -12,6 +12,7 @@ import Section from "../../../components/section";
 import {Col, Row} from "react-grid-system";
 import Title from "../../../components/ui/title";
 import {useTranslation} from "react-i18next";
+import Button from "../../../components/ui/button";
 
 const AgentsCommissionContainer = () => {
     const {t} = useTranslation()
@@ -30,17 +31,17 @@ const AgentsCommissionContainer = () => {
             path: '/agents/insurance-agents',
         }
     ], [])
-    const {data:agents,isLoading:isLoadingAgents} = useGetAllQuery({
-        key:KEYS.agents,url:`${URLS.agents}/list`,
-        params:{
-            params:{
+    const {data: agents, isLoading: isLoadingAgents} = useGetAllQuery({
+        key: KEYS.agents, url: `${URLS.agents}/list`,
+        params: {
+            params: {
                 branch: get(user, 'branch._id'),
-                limit: 100
+                limit: 150
             }
         }
     })
-    const {data:commission,isLoading:isLoadingCommission} = useGetAllQuery({
-        key:KEYS.agentCommission,url:`${URLS.agentCommission}/list`
+    const {data: commission, isLoading: isLoadingCommission} = useGetAllQuery({
+        key: KEYS.agentCommission, url: `${URLS.agentCommission}/list`
     })
 
     useEffect(() => {
@@ -50,39 +51,139 @@ const AgentsCommissionContainer = () => {
     return (
         <Section>
             <Row className={''} align={'center'}>
-                <Col xs={12}>
+                <Col xs={12} className={'mb-15'}>
                     <Title>{t('Комиссия и РПМ')}</Title>
                 </Col></Row>
 
-                <Row>
-                    <Col xs={12}>
-                        <Form formRequest={(val)=>console.log(val)}>
-                            <Table hideThead={false}
-                                   thead={['Наименование агента', 'Агентское вознаграждение(минимум (%))', 'Строго фиксирована', 'Введите фиксированное значение', 'Укажите тип франшизы', 'Укажите базу франшизы', 'Франшиза']}>
-                                {get(agents, 'data.data', []).map((item, i) => <tr key={i + 1}>
-                                    <td>
-                                        {get(item,'person.name') ? `${get(item,'person.secondname')} ${get(item,'person.name')} ${get(item,'person.middlename')}` : get(item,'organization.nameoforganization')}
-                                    </td>
+            <Row>
+                <Col xs={12} >
+                    <Form formRequest={(val) => console.log(val)} footer={ <Button className={'mt-30'} type={"submit"} lg>Установить выбранным
+                    </Button>}>
+                        <div className={'horizontal-scroll'}>
+                        <Table hideThead={false}
+                               thead={['Наименование агента', 'Агентское вознаграждение(минимум (%))', 'Агентское вознаграждение(максимум (%))', 'управлять удержанием при расторжении (y/n)', 'удерживать при расторжении (y/n)', 'Отчисление в РПМ(минимум (%))', 'Отчисление в РПМ(максимум (%))', 'можно изменять (y/n)','Штраф за порчу бланка (сум)','Штраф за утерю бланка (сум)']}>
+                            {get(agents, 'data.data', []).map((item, i) => <tr key={i + 1}>
+                                <td>
+                                    {get(item, 'person.name') ? `${get(item, 'person.secondname')} ${get(item, 'person.name')} ${get(item, 'person.middlename')}` : get(item, 'organization.nameoforganization')}
+                                </td>
 
-                                    <td>
-                                        <Flex justify={'center'}>
-                                            <Field
-                                                name={`agent[${i}].commission.minimumPercent`}
-                                                type={'number-format-input'}
-                                                property={{
-                                                    hideLabel: true,
-                                                    placeholder: 'ввод значения',
-                                                }}
-                                                defaultValue={0}
-                                            />
-                                        </Flex>
-                                    </td>
-
-                                </tr>)}
-                            </Table>
-                        </Form>
-                    </Col>
-                </Row>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'switch'}
+                                            property={{
+                                                hideLabel:true
+                                            }}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'switch'}
+                                            property={{
+                                                hideLabel:true
+                                            }}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'switch'}
+                                            property={{
+                                                hideLabel:true
+                                            }}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                                <td>
+                                    <Flex justify={'center'}>
+                                        <Field
+                                            name={`agent[${i}].commission.minimumPercent`}
+                                            type={'number-format-input'}
+                                            property={{
+                                                hideLabel: true,
+                                                placeholder: 'ввод значения',
+                                            }}
+                                            defaultValue={0}
+                                        />
+                                    </Flex>
+                                </td>
+                            </tr>)}
+                        </Table>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
 
         </Section>
     );
