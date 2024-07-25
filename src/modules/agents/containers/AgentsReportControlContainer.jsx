06d92bc13ce7b533d96351ec53292aa0976201fe
BoburUsmanoff <bobur@pixeel.co.uk>
2024-07-25 21:28:23 +0500

    fix

--- src/modules/agents/containers/AgentsReportControlContainer.jsx
+++ src/modules/agents/containers/AgentsReportControlContainer.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useMemo} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {useStore} from "../../../store";
-import {get, isEmpty} from "lodash";
+import {get, includes, isEmpty, isEqual} from "lodash";
 import {KEYS} from "../../../constants/key";
 import {URLS} from "../../../constants/url";
 import {useGetAllQuery, usePostQuery} from "../../../hooks/api";
@@ -13,9 +13,14 @@ import EmptyPage from "../../auth/pages/EmptyPage";
 import Table from "../../../components/table";
 import dayjs from "dayjs";
 import NumberFormat from "react-number-format";
+import {Trash2} from "react-feather";
+import Checkbox from "rc-checkbox";
+import Button from "../../../components/ui/button";
+import {ContentLoader} from "../../../components/loader";
 
 const AgentsReportControlContainer = () => {
     const {t} = useTranslation()
+    const [idList, setIdList] = useState([])
     const setBreadcrumbs = useStore(state => get(state, 'setBreadcrumbs', () => {
     }))
     const user = useStore(state => get(state, 'user'))
@@ -35,14 +40,38 @@ const AgentsReportControlContainer = () => {
     let {data: agentActList} = useGetAllQuery({key: KEYS.agentActList, url: URLS.agentActList})
 
 
-    const {mutate: actPostRequest, isLoading: isLoadingActPost} = usePostQuery({listKeyId: KEYS.policyByAgent})
+    const {mutate: setPaidRequest, isLoading} = usePostQuery({listKeyId: KEYS.agentActList})
 
+    const setPaid = () => {
+        setPaidRequest({
+            url: URLS.agentActStatus,
+            attributes: {
+                acts: idList,
+                isPaid: true
+            }
+        }, {
+            onSuccess: () => {
+                setIdList([])
+            }
+        })
+    }
+
+    const remove = (_id) => {
+        setPaidRequest({
+            url: URLS.agentActStatus,
+            attributes: {
+                acts: [_id],
+                isPaid: false
+            }
+        })
+    }
     useEffect(() => {
         setBreadcrumbs(breadcrumbs)
     }, [])
 
     return (
         <Section>
+            {isLoading && <ContentLoader/>}
             <Row className={''} align={'center'}>
                 <Col xs={12} className={'mb-15'}>
                     <Title>{t('Управления актами выполненных работ')}</Title>
@@ -51,21 +80,36 @@ const AgentsReportControlContainer = () => {
             <Row>
                 <Col xs={12}>
                     {isEmpty(get(agentActList, 'data.data', [])) ? <EmptyPage/> : <Table hideThead={false}
-                                                                                         thead={['Выбрать', '№', '№ акта', 'Дата акта', 'Агент', 'Сумма', 'Статус', 'Действие удалить']}>{get(agentActList, 'data.data', []).map((item, i) =>
+                                                                                         thead={[<Checkbox
+                                                                                             onChange={(e) => {
+                                                                                                 if (e.target?.checked) {
+                                                                                                     setIdList(get(agentActList, 'data.data', []).map(({_id}) => _id))
+                                                                                                 } else {
+                                                                                                     setIdList([])
+                                                                                                 }
+                                                                                             }}/>, '№', '№ акта', 'Дата акта', 'Агент', 'Сумма', 'Статус', 'Действие удалить']}>{get(agentActList, 'data.data', []).map((item, i) =>
                         <tr key={get(item, '_id')}>
-                            <td></td>
+                            <td><Checkbox checked={includes(idList, get(item, '_id'))} onChange={(e) => {
+                                if (e.target?.checked) {
+                                    setIdList(prev => ([...prev, get(item, '_id')]))
+                                } else {
+                                    setIdList(idList.filter(id => !isEqual(id, get(item, '_id'))))
+                                }
+                            }}/></td>
                             <td>{i + 1}</td>
                             <td>{get(item, 'actNumber')}</td>
                             <td>{dayjs(get(item, 'actDate')).format('DD.MM.YYYY')}</td>
-                            <td>{get(item, 'sender_name')}</td>
+                            <td> {get(item, 'agent.person.name') ? `${get(item, 'agent.person.secondname')} ${get(item, 'agent.person.name')} ${get(item, 'agent.person.middlename')}` : get(item, 'agent.organization.nameoforganization')}</td>
                             <td><NumberFormat displayType={'text'} thousandSeparator={" "}
-                                              value={get(item, 'payment_amount', 0)}/></td>
-                            <td>{get(item, 'payment_details')}</td>
-                            <td>{get(item, 'sender_taxpayer_number')}</td>
-                            <td>{get(item, 'recipient_bank_taxpayer_number')}</td>
-                            <td>{get(item, 'recipient_bank_code')}</td>
-                            <td>{get(item, 'recipient_bank_account')}</td>
+                                              value={get(item, 'totalPremiumAmount', 0)}/></td>
+                            <td>{get(item, 'status')}</td>
+                            <td>
+                                {get(item, 'status') === 'paid' &&
+                                <Trash2 onClick={() => remove(get(item, '_id', null))}
+                                        className={'ml-15 cursor-pointer'} color={'#dc2626'}/>}
+                            </td>
                         </tr>)}</Table>}
+                    <Button onClick={setPaid} className={"mt-30"}>{t("Установить")}</Button>
                 </Col>
             </Row>
         </Section>
