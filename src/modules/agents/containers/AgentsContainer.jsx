2621128cc9ade521e10203a658e4396511caad06
Bobur <boburusmanov1994@gmail.com>
2025-03-04 11:20:53 +0500

    fix:filter

--- src/modules/agents/containers/AgentsContainer.jsx
+++ src/modules/agents/containers/AgentsContainer.jsx
@@ -1,20 +1,31 @@
-import React, {useEffect, useMemo} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {useStore} from "../../../store";
-import {get, isEqual} from "lodash";
+import {get, includes, isEqual} from "lodash";
 import GridView from "../../../containers/grid-view/grid-view";
 import {KEYS} from "../../../constants/key";
 import {URLS} from "../../../constants/url";
-import {Download, RefreshCcw} from "react-feather";
-import {usePostQuery} from "../../../hooks/api";
+import {Download, FileText, Filter, RefreshCcw, Trash} from "react-feather";
+import {useGetAllQuery, usePostQuery} from "../../../hooks/api";
 import {ContentLoader} from "../../../components/loader";
 import config from "../../../config";
 import { useTranslation } from 'react-i18next';
+import dayjs from "dayjs";
+import Form from "../../../containers/form/form";
+import {Col, Row} from "react-grid-system";
+import Field from "../../../containers/form/field";
+import Button from "../../../components/ui/button";
+import Flex from "../../../components/flex";
+import {getSelectOptionsListFromData} from "../../../utils";
 
-const AgentsContainer = ({...rest}) => {
+const AgentsContainer = () => {
 
     const setBreadcrumbs = useStore(state => get(state, 'setBreadcrumbs', () => {
     }))
+    const user = useStore(state => get(state, 'user'))
     const {t} = useTranslation()
+    const [filter, setFilter] = useState({
+        // branch: get(user, 'branch._id'),
+    });
     const {mutate:generateAgentAgreement,isLoading} = usePostQuery({listKeyId:[KEYS.agents,1]})
 
     const breadcrumbs = useMemo(() => [
@@ -29,6 +40,14 @@ const AgentsContainer = ({...rest}) => {
             path: '/agents/insurance-agents',
         }
     ], [])
+    let {data: branches} = useGetAllQuery({
+        key: KEYS.branches, url: `${URLS.branches}/list`, params: {
+            params: {
+                limit: 200
+            }
+        }
+    })
+    branches = getSelectOptionsListFromData(get(branches, `data.data`, []), '_id', 'branchName')
 
     useEffect(() => {
         setBreadcrumbs(breadcrumbs)
@@ -79,6 +98,17 @@ const AgentsContainer = ({...rest}) => {
                         title: t("Middlename")
                     },
                     {
+                        id: 557,
+                        key: 'agreementnumber',
+                        title: t("agreementnumber")
+                    },
+                    {
+                        id: 558,
+                        key: 'agreementdate',
+                        title: t("agreementdate"),
+                        render:(_tr)=>dayjs(get(_tr,'agreementdate')).format("DD-MM-YYYY")
+                    },
+                    {
                         id: 555,
                         key: 'agreementPath',
                         title: t("Agreement file"),
@@ -87,7 +117,7 @@ const AgentsContainer = ({...rest}) => {
                         }
                     },
                 ]}
-                keyId={KEYS.agents}
+                keyId={[KEYS.agents,filter]}
                 url={URLS.agents}
                 listUrl={`${URLS.agents}/list`}
                 title={t("Страховые агенты")}
@@ -95,7 +125,34 @@ const AgentsContainer = ({...rest}) => {
                 createUrl={'/agents/create'}
                 updateUrl={'/agents/update'}
                 isHideColumn
+                params={{...filter}}
                 hasUpdateBtn={true}
+                extraFilters={<Form sm formRequest={({data: {group, subGroup, ...rest} = {}}) => {
+                    setFilter(rest);
+                }}
+                                   mainClassName={'mt-15'}>
+
+                    {() => <Row align={'end'} gutterWidth={10}>
+                        <Col xs={4}>
+                            <Field  type={'select'}
+                                   label={t("Филиал")} name={'branch'}
+                                   options={branches} defaultValue={get(filter, 'branch')}
+                                   isDisabled={!includes([config.ROLES.admin], get(user, 'role.name'))}/>
+                        </Col>
+
+                        <Col xs={3}>
+                            <div style={{display:'flex',marginBottom:'12px',marginLeft:'15px'}}>
+                                <Button  htmlType={'submit'}><Flex justify={'center'}><Filter size={14}/><span
+                                    style={{marginLeft: '5px'}}>{t("Применить")}</span></Flex></Button>
+                                <Button className={'ml-15'}  onClick={() => {
+                                    setFilter({})
+                                }}  danger type={'reset'}><Flex justify={'center'}><Trash
+                                    size={14}/><span
+                                    style={{marginLeft: '5px'}}>{t("Очистить")}</span></Flex></Button>
+                            </div>
+                        </Col>
+                    </Row>}
+                </Form>}
             />
         </>
     );
